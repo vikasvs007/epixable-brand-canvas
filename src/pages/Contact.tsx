9c1b317078c21cd9b53cfaeedb0ce2a3becc514c
Vikas V S <vikasvs6363163@gmail.com>
2025-07-13 20:29:55 +0530

    hskdfhksjd

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -158,7 +158,7 @@ const Contact = () => {
               <Sparkles className="w-5 h-5 mr-3 text-[#90D2B5]" />
               <span className="text-sm font-semibold text-white">Get In Touch</span>
             </div>
-            
+            {/* //// sfdahs asuyfajshbf jsbfjhabsfjbsafj jhsabfjhbasfbasf jhasbfjh */}
             <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold mb-8 leading-tight animate-fade-in-up">
               Let's Start
               <span className="block text-gradient mt-4">
